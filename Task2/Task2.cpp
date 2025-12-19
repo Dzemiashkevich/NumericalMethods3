31394f8895209566b069ebbc231a68e0e385a554
Dzemiashkevich <vladdemiashkevich@gmail.com>
2025-12-19 04:25:03 +0300

    Move png images to directory Graphics

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -12,7 +12,8 @@ int main () {
     std::function<double(double)> f2 = [](double x) {return 1/(cos(x+1)+sin(x+1));};
     double a=0;
     double b=2;
-    double app_err=1e-8;
+    double app_err=1e-10;
+    std::cout<<std::setprecision(12)<<std::fixed;
     std::cout<<Sympson<double>::answer(a,b, f, 1,app_err)<<"\n"<<Trapeze<double>::answer(a,b, f, 1,app_err )<<"\n"<<CentralRect<double>::answer(a,b, f, 1,app_err)<<"\n"<<Newton_Kotes<double>::answer(a,b,f, 0, 12)<<"\n";
     std::cout<<GaussLeg<double>::answer(5,f2);
     return 0;
